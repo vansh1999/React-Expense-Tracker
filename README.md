3a0c7ac2a6d6312ffb712823d71052e39e1d4f7e
vansh1999 <vansh.bhardwaj1999@gmail.com>
2021-01-16 16:49:55 +0530

    expense tracker

---
+++ README.md
@@ -0,0 +1,1 @@
+Expense Tracker System is a system which will keep a track of your Income-Expense.
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h2>
+            Expense tracker
+        </h2>
+    )
+}
